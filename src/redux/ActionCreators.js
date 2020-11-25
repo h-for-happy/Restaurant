9e7237ed6fdb915528ee2f9f6616e28c31b8be24
Hitesh Taneja <hiteshtaneja307@gmail.com>
2020-11-26 00:53:04 +0530

    Assignment 4 Completed

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -146,3 +146,84 @@ export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos
 });
+
+
+
+
+//LEADERS
+
+export const fetchLeaders = () => (dispatch) => {
+    
+    dispatch(leadersLoading());
+
+    return fetch(baseUrl + 'leaders')
+    .then(response => {
+        if(response.ok){
+            return response;
+        }else{
+            var error = new Error ('Error ' + response.status + ': ' + response.statusText );
+            error.response = response;
+            throw error;
+            }
+        },
+        error => {
+            var errmess = new Error (error.message);
+            throw errmess;
+        
+        })
+    .then(response => response.json())
+    .then(leaders => dispatch(addLeaders(leaders)))
+    .catch(error => dispatch(leadersFailed(error.message)));
+}
+
+export const leadersLoading = () => ({
+    type: ActionTypes.LEADERS_LOADING
+});
+
+export const leadersFailed = (errmess) => ({
+    type: ActionTypes.LEADERS_FAILED,
+    payload: errmess
+});
+
+export const addLeaders = (leaders) => ({
+    type: ActionTypes.ADD_LEADERS,
+    payload: leaders
+});
+
+//Feedback
+
+export const postFeedback = (feedback) => (dispatch) => {
+    const newFeedback = Object.assign({ date: new Date().toISOString() }, feedback)
+
+    return fetch(baseUrl + 'feedback', {
+        method: 'POST',
+        body: JSON.stringify(newFeedback),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+    })
+    .then((response) => {
+        if(response.ok) {
+            
+            return response;
+            
+        } else {
+            var error = new Error('Error' + response.status + ': ' + response.statusText);
+            error.response = response;
+
+            throw error;
+        }
+    }, (error) => {
+        var errorMessage = new Error(error.errorMessage);
+        throw errorMessage;
+    })
+    .then((response) => response.json())
+    .then(response =>
+        alert("Thank you for your feedback!" + JSON.stringify(response))
+      )
+    .catch(error => {
+        console.log('POST feedback' + error.message);
+        alert('Feedback could not be posted:\n' + error.message);
+    })
+}
